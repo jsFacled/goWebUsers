88bb812cb3f27b467af3d19b5d969e4ef9239611
faGitLab <facled@yahoo.com.ar>
2024-06-01 07:21:00 -0300

    capa repository

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -6,14 +6,16 @@ import (
 	"log"
 )
 
-// Generamos un esquema de Base de Datos
+// Generamos un esquema de Base de Datos.
+//DB es una estructura que simula una base de datos en memoria
 type DB struct {
-	Users     []domain.User
+	Users     []domain.User	//slice de usuarios
 	MaxUserID uint64
 }
 
 type (
-	// Interfaz para definir metodos
+	// Interfaz para definir metodos.
+	//Cualquier tipo que implemente estos métodos puede considerarse un Repository.
 	Repository interface {
 		Create(ctx context.Context, user *domain.User) error
 		GetAll(ctx context.Context) ([]domain.User, error)
@@ -32,14 +34,19 @@ func NewRepo(db DB, l *log.Logger) Repository {
 	}
 }
 
+// - - - Métodos de repo - - - 
+
+//El asterisco* permite ingresar a la instancia repo y modificarla, lo mismo con User
+//Aquí no estoy creando un usuario, simplemente agrego un user a la BD el cual vendrá por parámetro
 func (r *repo) Create(ctx context.Context, user *domain.User) error{
-	r.db.MaxUserID++
-	user.ID = r.db.MaxUserID
-	r.db.Users = append(r.db.Users, *user)
-	r.log.Println("repository create")
-	return nil
+	r.db.MaxUserID++						//Accede al campo db de repo y aumenta el id
+	user.ID = r.db.MaxUserID				//asigna el valor del nuevo id
+	r.db.Users = append(r.db.Users, *user)	//agrega el user al slice
+	r.log.Println("repository create")		//registra un mensaje en el log de repo 
+	return nil								//Devuelve nil si está todo ok
 }
 
+//Devuelve el slice de usuarios almacenados y nil para indicar que no hubo errores.
 func (r *repo) GetAll(ctx context.Context) ([]domain.User, error){
 r.log.Println("repositoryget all")
 return r.db.Users, nil
