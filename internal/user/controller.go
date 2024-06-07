189ab786d9efa27e79093c94ad78484cc7eb56f2
faGitLab <facled@yahoo.com.ar>
2024-06-07 09:24:33 -0300

    capa controller metodo PostUser

--- internal/user/controller.go
+++ internal/user/controller.go
@@ -1,9 +1,16 @@
 package user
 
+/*
+Video:
+- Hasta el minuto 8 se define el mètodo GetAllUser
+- 8´: metodo Post.
+
+
+*/
 import (
-	"fmt"
 	"context"
 	"encoding/json"
+	"fmt"
 	"goWebUsers/internal/domain"
 	"net/http"
 )
@@ -26,8 +33,10 @@ type (
 func MakeEndpoints(ctx context.Context, s Service) Controller {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
+
 		case http.MethodGet:
 			GetAllUser(ctx, s, w)
+
 		case http.MethodPost:
 			decode := json.NewDecoder(r.Body)
 			var user domain.User
@@ -36,6 +45,7 @@ func MakeEndpoints(ctx context.Context, s Service) Controller {
 				return
 			}
 			PostUser(ctx, s, w, user)
+
 		default:
 			InvalidMethod(w)
 		}
@@ -51,6 +61,30 @@ func GetAllUser(ctx context.Context, s Service, w http.ResponseWriter) {
 	DataResponse(w, http.StatusOK, users)
 }
 
+// Método
+func PostUser(ctx context.Context, s Service, w http.ResponseWriter, data interface{}) {
+	req := data.(CreateReq)
+
+	if req.FirstName == "" {
+		MsgResponse(w, http.StatusBadRequest, "first name is required")
+		return
+	}
+	if req.LastName == "" {
+		MsgResponse(w, http.StatusBadRequest, "last name is required")
+		return
+	}
+	if req.Email == "" {
+		MsgResponse(w, http.StatusBadRequest, "email is required")
+		return
+	}
+
+	user, err := s.Create(ctx, req.FirstName, req.LastName, req.Email)
+	if err != nil {
+		MsgResponse(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	DataResponse(w, http.StatusCreated, user)
+}
 
 // Método que da un mensaje en la Respuesta
 func MsgResponse(w http.ResponseWriter, status int, message string) {
